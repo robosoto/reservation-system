f69813716de05ed5e4394dde5fcc1a2fe99ce937
AndyChallman <challmana@gmail.com>
2023-09-15 16:36:26 -0400

    ChallmanEvening7/15
    Adding files

---
+++ reservation-system-frontend/src/app/user-info-form/user-info-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-info-form',
+  templateUrl: './user-info-form.component.html',
+  styleUrls: ['./user-info-form.component.css']
+})
+export class UserInfoFormComponent {
+
+}
