83bb72f143702807a566ae415bc5b6df640c9d5f
Roberto Soto <rsoto1121@gmail.com>
2023-09-15 18:08:14 -0400

    basic mock data

---
+++ database/reservations_mock_data.sql
@@ -0,0 +1,5 @@
+insert into reservations (id, start_date, end_date, location, customer_id) values (1, '2023-09-21 12:51:44', '2023-09-22 21:23:20', 'Philadelphia', 1);
+insert into reservations (id, start_date, end_date, location, customer_id) values (2, '2023-09-19 11:56:39', '2023-09-23 13:40:19', 'Philadelphia', 5);
+insert into reservations (id, start_date, end_date, location, customer_id) values (3, '2023-09-22 08:18:34', '2023-09-29 02:56:24', 'Philadelphia', 2);
+insert into reservations (id, start_date, end_date, location, customer_id) values (4, '2023-09-24 07:50:59', '2023-09-21 14:48:50', 'Philadelphia', 4);
+insert into reservations (id, start_date, end_date, location, customer_id) values (5, '2023-09-24 09:01:57', '2023-09-29 10:09:59', 'Philadelphia', 2);
