020bdbd801f7832876cc9a5c057bb39349637898
Roberto Soto <rsoto1121@gmail.com>
2023-09-16 23:07:58 -0400

    add vehicle data service

--- reservation-system-frontend/src/app/services/vehicle.service.ts
+++ reservation-system-frontend/src/app/services/vehicle.service.ts
@@ -2,16 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Vehicle } from '../types/vehicle';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehicleService {
 
+  private vehicleUrl = environment.vehicleApiUrl;
+
   constructor(private http: HttpClient) { }
 
   getAllVehicles(): Observable<Vehicle[]> {
-    return this.http.get<Vehicle[]>(this.)
+    const url = `${this.vehicleUrl}/all`;
+    return this.http.get<Vehicle[]>(url);
+  }
+
+  getVehiclesByLocation(location: string): Observable<Vehicle[]> {
+    const url = `${this.vehicleUrl}/location/${location}`;
+    return this.http.get<Vehicle[]>(url);
+  }
+
+  getVehiclesByType(type: string): Observable<Vehicle[]> {
+    const url = `${this.vehicleUrl}/type/${type}`;
+    return this.http.get<Vehicle[]>(url);
   }
 
 }
