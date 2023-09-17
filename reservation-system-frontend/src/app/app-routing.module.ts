b518969959953779c0957765ee44cde8cc57b177
Roberto Soto <rsoto1121@gmail.com>
2023-09-16 23:08:30 -0400

    initial changes to display list of all vehicles

--- reservation-system-frontend/src/app/app-routing.module.ts
+++ reservation-system-frontend/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
+import { VehiclesComponent } from './available-vehicles/vehicles.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  }
+  { path: '', component: HomeComponent },
+  { path: 'vehicle/all', component: VehiclesComponent }
 ];
 
 @NgModule({
