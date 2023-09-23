db9a6e49424800773fc3caa0d82d7963bb1c0824
swtyshaa <swty.saha@gmail.com>
2023-09-23 14:03:07 -0400

    i18 changes

--- reservation-system-frontend/src/app/available-vehicles/vehicles.component.ts
+++ reservation-system-frontend/src/app/available-vehicles/vehicles.component.ts
@@ -5,6 +5,8 @@ import { Vehicle } from '../types/vehicle';
 import { VehicleService } from '../services/vehicle.service';
 import { environment } from 'src/environments/environment.development';
 import { DropdownChangeEvent } from 'primeng/dropdown';
+import { TranslateService } from '@ngx-translate/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-vehicles',
@@ -20,8 +22,12 @@ export class VehiclesListComponent {
   paginatorPageNum: number = 1;
 
   constructor(
+    public translate: TranslateService,
+    public primeNGConfig: PrimeNGConfig,
     private vehicleService: VehicleService
-  ) {}
+    
+  ){} 
+  
 
   getAllVehicles(): void {
     this.vehicleService.getAllVehicles().subscribe(vehicles => this.vehicles = vehicles);
--- reservation-system-frontend/src/app/reservation-form/reservation.component.ts
+++ reservation-system-frontend/src/app/reservation-form/reservation.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { PrimeNGConfig } from 'primeng/api';
+import { Subscription } from 'rxjs';
 
 
 interface Location {
   name: string;
   code: string;
 }
+
 
 @Component({
   selector: 'app-reservation',
@@ -20,7 +24,16 @@ export class ReservationComponent  {
   rangeDates: Date[] | undefined;
 
   value: string | undefined;
-
+  subscription: Subscription;
+  constructor(
+    public translate: TranslateService,
+    public primeNGConfig: PrimeNGConfig
+    
+  ) {
+    this.subscription = this.translate.stream('primeng').subscribe(data => {
+      this.primeNGConfig.setTranslation(data);
+    });
+  }
   ngOnInit() {
     this.location = [
         { name: 'Philadelphia', code: 'PHI' },
