dccda1b8d531a4b9ed255281a1de82fd389df46a
Roberto Soto <rsoto1121@gmail.com>
2023-09-24 17:14:49 -0400

    update reservations table ddl

--- database/car_rental_reservations.ddl.sql
+++ database/car_rental_reservations.ddl.sql
@@ -1,10 +1,14 @@
 CREATE TABLE `reservations` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
+  `id` varchar(30) NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
   `location` varchar(80) NOT NULL,
   `customer_id` int NOT NULL,
+  `vehicle_id` int NOT NULL,
+  `status` varchar(30) DEFAULT 'Reserved',
   PRIMARY KEY (`id`),
   KEY `customer_id_idx` (`customer_id`),
-  CONSTRAINT `customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `vehicle_id_idx` (`vehicle_id`),
+  CONSTRAINT `customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `vehicle_id` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicles` (`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
