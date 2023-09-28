777473d1511fb6b066dcdc1bd54e650535d101c4
swtyshaa <swty.saha@gmail.com>
2023-09-28 16:17:32 -0400

    Localization changes for home and location

--- reservation-system-frontend/src/app/home/home.component.ts
+++ reservation-system-frontend/src/app/home/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  constructor(
+    public translate: TranslateService
+  )
+  {
 
+  }
 }
