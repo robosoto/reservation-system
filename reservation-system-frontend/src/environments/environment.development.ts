1bf3bcbae8ce8b375f2096b9ab8055fbdde88597
Roberto Soto <rsoto1121@gmail.com>
2023-09-28 20:45:12 -0400

    update locations in typescript environments file

--- reservation-system-frontend/src/environments/environment.development.ts
+++ reservation-system-frontend/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     apiUrl: "http://project2-goeazy-carrent-env.eba-xjdmbrhh.us-east-1.elasticbeanstalk.com:8080",
-    locations: ["Philadelphia", "Mumbai"],
+    locations: ["Philadelphia", "Cancún"],
     vehicleTypes: ["Car", "SUV", "Truck", "Van"]
 };
