4a047e15a872a5c54f2dacb991d8f56c75255cb8
Roberto Soto <rsoto1121@gmail.com>
2023-10-01 23:13:23 -0400

    dollars to pesos conversion on available-vehicle table

--- reservation-system-frontend/src/environments/environment.development.ts
+++ reservation-system-frontend/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     apiUrl: "http://project2-goeazy-carrent-env.eba-xjdmbrhh.us-east-1.elasticbeanstalk.com:8080",
     locations: ["Philadelphia", "Mumbai"],
-    vehicleTypes: ["Car", "SUV", "Truck", "Van"]
+    vehicleTypes: ["Car", "SUV", "Truck", "Van"],
+    dollarsToPesosMultiplier: 17.40
 };
