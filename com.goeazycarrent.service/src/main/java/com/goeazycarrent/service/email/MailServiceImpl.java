5715a8c3bded0b616f7dcac73533eb69c0d82fc0
Roberto Soto <rsoto1121@gmail.com>
2023-10-02 19:57:11 -0400

    change java version and dependencies to java 8 in order to run tests with java 8 jdk

--- com.goeazycarrent.service/src/main/java/com/goeazycarrent/service/email/MailServiceImpl.java
+++ com.goeazycarrent.service/src/main/java/com/goeazycarrent/service/email/MailServiceImpl.java
@@ -5,9 +5,9 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.InternetAddress;
-import jakarta.mail.internet.MimeMessage;
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class MailServiceImpl implements MailService
