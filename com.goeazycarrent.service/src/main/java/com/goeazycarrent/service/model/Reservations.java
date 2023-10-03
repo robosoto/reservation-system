e1b18b4e93b07a0406c321bfad29cc4e0dc630f6
Roberto Soto <rsoto1121@gmail.com>
2023-10-03 12:55:03 -0400

    Revert "change java version and dependencies to java 8 in order to run tests with java 8 jdk"
    This reverts commit 5715a8c3bded0b616f7dcac73533eb69c0d82fc0.

--- com.goeazycarrent.service/src/main/java/com/goeazycarrent/service/model/Reservations.java
+++ com.goeazycarrent.service/src/main/java/com/goeazycarrent/service/model/Reservations.java
@@ -3,11 +3,11 @@ package com.goeazycarrent.service.model;
 import java.util.Date;
 import java.util.Objects;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Reservations {
